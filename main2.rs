349587ace36f6be0dd80da707e35812f1aa37acd
Abraham <akpanabraham001@gmail.com>
2024-11-23 13:06:23 +0100

    Rust arithmetic ops, user-input and typecasting

---
+++ main2.rs
@@ -0,0 +1,46 @@
+//use std::io;
+fn main(){
+  // accepting user input
+   /*   println!("enter ur name: ");
+        let mut input = String::new();
+        io::stdin()
+                .read_line(&mut input)
+                .expect("failed to read line");
+        println!("ur name is: {}",input);
+  */
+   /*  //arithmetic operation   
+        let _x: u8 = 4; // 0 - 255
+        let _y: i8 = 15; // -128 - 127
+
+        let a: i8 = 4;// for addition, the numbers must be of the exact same da>
+        let b: i8 = 15;
+
+        let d: f32 = 270.4;
+    	let e: f32 = 100.0;
+
+        let k = d / e;
+        let c = a / b;
+        println!("The result for k is: {} while c is: {}", k,c)
+    */
+    /* //type casting and conversion
+        let _x: f32 = 270.4;// could also be written as let d = 270.4f32;
+        let _y: i8 = 15; // same as let b = 15i8; 0r let b = 15_i8; 
+
+        let a = 127_000 as i64;// or 127_000_i64
+        let b = 10_i32;// or let b = 10 as i32;
+        let d = (i32::MAX as f32) + 1.0;
+        let c = (a as i32) / b;
+
+        println!("result for d and c are:{} and {} respectively",d,c)
+   */
+    //type casting in user input
+    /*	println!("enter ur current age below");
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).expect("expected to read line");
+	
+	let int_input: i64 = input.trim().parse().unwrap();
+
+	println!("ur age in two yrs time is: {}", int_input + 2);
+    */
+   
+}
